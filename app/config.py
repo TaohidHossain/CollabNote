412861b79902efc1bb1632ab1a6a6f4eaeb0f208
Taohid Hossain <htaohid31@gmail.com>
2026-05-21 10:09:10 +0600

    chore: add app configuration

---
+++ app/config.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+_base_config = SettingsConfigDict(
+    env_file=".env",
+    env_file_encoding="utf-8",
+    extra="ignore",
+    env_ignore_empty=True,
+    )
+
+
+class DB_Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: str
+    POSTGRES_DATABASE: str
+
+    model_config = _base_config
+
+class JWT_Settings(BaseSettings):
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    model_config = _base_config
+
+
+db_settings = DB_Settings()
+jwt_settings = JWT_Settings()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+
+from app.config import db_settings, jwt_settings
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "message": "Hello World",
+        "db_settings": {
+            "POSTGRES_USER": db_settings.POSTGRES_USER,
+            "POSTGRES_PASSWORD": db_settings.POSTGRES_PASSWORD,
+            "POSTGRES_SERVER": db_settings.POSTGRES_SERVER,
+            "POSTGRES_PORT": db_settings.POSTGRES_PORT,
+            "POSTGRES_DATABASE": db_settings.POSTGRES_DATABASE,
+        },
+        "jwt_settings": {
+            "JWT_SECRET": jwt_settings.JWT_SECRET,
+            "JWT_ALGORITHM": jwt_settings.JWT_ALGORITHM,
+            "JWT_ACCESS_TOKEN_EXPIRE_MINUTES": jwt_settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES,
+        }
+    }
